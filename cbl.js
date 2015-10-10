24c6a7c50decd4f3da5b5260a73a9e3f5e2ebb98
Scott Clayton <scottclayton84+github@gmail.com>
2015-10-10 16:00:10 -0500

    Added model compression

--- cbl.js
+++ cbl.js
@@ -1,17 +1,25 @@
 /*
- * CBL-js (CAPTCHA Breaking Library in JavaScript)
+ * CBL-js
+ * CAPTCHA Breaking Library in JavaScript
+ * https://github.com/skotz/cbl-js
  * Copyright (c) 2015 Scott Clayton
  */
  
 var CBL = function (options) {
 
     var defaults = {
-        preprocess: function() { console.warn("CBL: You should define a preprocess method!"); },        
+        preprocess: function() { warn("You should define a preprocess method!"); },   
+        model_file: "",
+        model_string: "",
+        model_loaded: function() { },
         blob_min_pixels: 1,
         blob_max_pixels: 99999,
         pattern_width: 20,
         pattern_height: 20,
-        blob_debug: ""
+        blob_debug: "",
+        allow_console_log: false,
+        allow_console_warn: true,
+        character_set: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     };
 
     options = options || {};
@@ -64,9 +72,11 @@ var CBL = function (options) {
                     // Set up a list of patterns for a human to classify
                     if (typeof patternElementID !== 'undefined' && typeof humanSolutionElementID !== 'undefined' && blobs.length) {
                         for (var i = 0; i < blobs.length; i++) {
+                            var imgUrl = blobs[i].toDataURL();
+                            var blobPattern = blobToPattern(blobs[i]);
                             pendingPatterns.push({
-                                imgSrc: blobs[i].toDataURL(),
-                                pattern: blobToPattern(blobs[i]),
+                                imgSrc: imgUrl,
+                                pattern: blobPattern,
                                 imgId: patternElementID,
                                 txtId: humanSolutionElementID,
                                 self: obj,
@@ -75,7 +85,10 @@ var CBL = function (options) {
                         }
                         
                         // Load first pattern
-                        obj.loadNextPattern();
+                        if (!currentlyTraining) {
+                            obj.loadNextPattern();
+                        }
+                        currentlyTraining = true;
                     }
                     
                     // FOR SOLVING
@@ -84,7 +97,7 @@ var CBL = function (options) {
                         for (var i = 0; i < blobs.length; i++) {
                             solution += findBestMatch(blobToPattern(blobs[i]));
                         }
-                        console.log("CBL: Solution = " + solution)
+                        log("Solution = " + solution);
                     }
                         
                     obj.reset();
@@ -95,6 +108,10 @@ var CBL = function (options) {
             }
             else {
                 image.onload = afterLoad();
+                
+                // // Stuff breaks if we continue without having finished loading the image...
+                // var start = new Date().getTime();
+                // while (locked && new Date().getTime() - start < 5000);
             }
             return solution;
         },
@@ -103,33 +120,41 @@ var CBL = function (options) {
         loadNextPattern: function() {
             var nextPattern = pendingPatterns.pop();
             if (nextPattern) {
-                console.log("CBL: Loading a pattern for human classification.");
+                log("Loading a pattern for human classification.");
                 document.getElementById(nextPattern.imgId).src = nextPattern.imgSrc;
                 document.getElementById(nextPattern.txtId).focus();
-                document.getElementById(nextPattern.txtId).onkeyup = function() {
-                    model.push({
-                        pattern: nextPattern.pattern,
-                        solution: document.getElementById(nextPattern.txtId).value
-                    });
-                    console.log("CBL: Added \"" + document.getElementById(nextPattern.txtId).value + "\" pattern to model!");
-                    document.getElementById(nextPattern.txtId).value = "";
-                    
-                    // Load the next pattern
-                    if (pendingPatterns.length) {
-                        nextPattern.self.loadNextPattern();
+                document.getElementById(nextPattern.txtId).onkeyup = function(event) {
+                    var typedLetter = document.getElementById(nextPattern.txtId).value;
+                    if (options.character_set.indexOf(typedLetter) > -1 && typedLetter.length) {
+                        model.push({
+                            pattern: nextPattern.pattern,
+                            solution: document.getElementById(nextPattern.txtId).value
+                        });
+                        log("Added \"" + document.getElementById(nextPattern.txtId).value + "\" pattern to model!");
+                        document.getElementById(nextPattern.txtId).value = "";
+                        
+                        // Load the next pattern
+                        if (pendingPatterns.length) {
+                            nextPattern.self.loadNextPattern();
+                        }
+                        else {
+                            currentlyTraining = false;
+                            document.getElementById(nextPattern.txtId).onkeyup = function () { };
+                            if (typeof nextPattern.onComplete === 'function') {
+                                nextPattern.onComplete();
+                            }
+                        }
                     }
                     else {
-                        document.getElementById(nextPattern.txtId).onkeyup = function () { };
-                        if (typeof nextPattern.onComplete === 'function') {
-                            nextPattern.onComplete();
-                        }
+                        document.getElementById(nextPattern.txtId).value = "";
                     }
                 };
             } 
         },
         
         // Load a model by deserializing a model string
-        loadModel: function (modelString) {
+        loadModelString: function (modelString) {
+            modelString = LZString.decompressFromBase64(modelString);
             model = new Array();
             var patterns = modelString.replace(/\[/g, "").split("]");
             for (var i = 0; i < patterns.length; i++) {
@@ -143,7 +168,30 @@ var CBL = function (options) {
                     });
                 }
             }
-            console.log("CBL: Model loaded with " + model.length + " patterns!")
+            if (!model.length) {
+                warn("No patterns to load in provided model.");    
+            }
+            else {
+                log("Model loaded with " + model.length + " patterns!");
+                options.model_loaded();
+            }
+        },
+        
+        // Load a model from a file on the server
+        loadModel: function (url) {
+            try {
+                var xhr = new XMLHttpRequest();
+                xhr.open("GET", url, true);
+                xhr.send();
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState == 4 && xhr.status == 200 && xhr.responseText) {
+                        obj.loadModelString(xhr.responseText);    
+                    }
+                }
+            }
+            catch (err) {
+                warn("Could not load model from \"" + url + "\"! (" + err.message + ")");
+            }
         },
         
         // Serialize the model
@@ -152,7 +200,54 @@ var CBL = function (options) {
             for (var i = 0; i < model.length; i++) {
                 str += "[" + model[i].solution + "=" + model[i].pattern + "]";
             }
+            str = LZString.compressToBase64(str);
             return str;
+        },
+        
+        // Debug stuff about the model
+        debugModel: function () {
+            for (var i = 0; i < model.length; i++) {
+                log(model[i].solution + " pattern length = " + model[i].pattern.split(".").length);
+            }
+        },
+        
+        // Condense the model by combining patterns with the same solution
+        condenseModel: function () {
+            var newModel = new Array();
+            var oldCount = model.length;
+            for (var i = 0; i < model.length; i++) {
+                var patternArray = model[i].pattern.split(".");
+                var found = false;
+                for (var j = 0; j < newModel.length; j++) {
+                    // These two patterns have the same solution, so combine the patterns
+                    if (newModel[j].solution == model[i].solution) {
+                        for (var x = 0; x < newModel[j].tempArray.length; x++) {
+                            newModel[j].tempArray[x] = parseInt(newModel[j].tempArray[x]) + parseInt(patternArray[x]);
+                        }
+                        newModel[j].tempCount++;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    newModel.push({
+                        pattern: model[i].pattern,
+                        solution: model[i].solution,
+                        tempArray: patternArray,
+                        tempCount: 1
+                    });
+                }
+            }
+            // Normalize the patterns
+            for (var i = 0; i < newModel.length; i++) {
+                for (var x = 0; x < newModel[i].tempArray.length; x++) {
+                    newModel[i].tempArray[x] = Math.round(newModel[i].tempArray[x] / newModel[i].tempCount);
+                }
+                newModel[i].pattern = newModel[i].tempArray.join(".");
+            }
+            model = newModel;
+            log("Condensed model from " + oldCount + " patterns to " + model.length + " patterns!");
+            return this;
         },
         
         // Unload an image
@@ -268,7 +363,7 @@ var CBL = function (options) {
                     blobs.push(blob);
                         
                     if (typeof debugElement !== 'undefined' && debugElement.length) {
-                        console.log("CBL: Blob size = " + pixels);
+                        log("Blob size = " + pixels);
                         var test = document.createElement("img");
                         test.src = blob.toDataURL();
                         // test.border = 1;
@@ -397,6 +492,7 @@ var CBL = function (options) {
     
     var model = new Array();
     var pendingPatterns = new Array();
+    var currentlyTraining = false;
     
     var locked = false;
     var canvas;
@@ -406,6 +502,7 @@ var CBL = function (options) {
         var best = 4000000000;
         var solution = "?";
         for (var i = 0; i < model.length; i++) {
+            log(model[i].pattern.length + " - " + pattern.length)
             var test = getPatternDifference(model[i].pattern, pattern);
             if (test < best) {
                 best = test;
@@ -418,7 +515,7 @@ var CBL = function (options) {
     // Convert a blob to a pattern object
     var blobToPattern = function (blob) {
         var pattern = new Array();
-        var image = blob.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+        var image = blob.getContext('2d').getImageData(0, 0, blob.width, blob.height);
         for (var x = 0; x < image.width; x++) {
             for (var y = 0; y < image.height; y++) {
                 var i = x * 4 + y * 4 * image.width;
@@ -521,6 +618,28 @@ var CBL = function (options) {
         while ((b = Math.round(Math.random() * 200) + 55) == r || b == g);
         return obj.color(r, g, b);
     };
+    
+    var log = function (message) {
+        if (options.allow_console_log) {
+            console.log("CBL: " + message);
+        }  
+    };
+    
+    var warn = function (message) {
+        if (options.allow_console_warn) {
+            console.warn("CBL: " + message);
+        }
+    };
+    
+    // Load the model
+    if (options.model_file.length) {
+        obj.loadModel(options.model_file);
+    } else if (options.model_string.length) {
+        obj.loadModelString(options.model_string);
+    }
+    
+    // ZIP compression from https://github.com/pieroxy/lz-string
+    var LZString=function(){function o(o,r){if(!t[o]){t[o]={};for(var n=0;n<o.length;n++)t[o][o.charAt(n)]=n}return t[o][r]}var r=String.fromCharCode,n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",e="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",t={},i={compressToBase64:function(o){if(null==o)return"";var r=i._compress(o,6,function(o){return n.charAt(o)});switch(r.length%4){default:case 0:return r;case 1:return r+"===";case 2:return r+"==";case 3:return r+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(e){return o(n,r.charAt(e))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(o){return null==o?"":""==o?null:i._decompress(o.length,16384,function(r){return o.charCodeAt(r)-32})},compressToUint8Array:function(o){for(var r=i.compress(o),n=new Uint8Array(2*r.length),e=0,t=r.length;t>e;e++){var s=r.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null===o||void 0===o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;t>e;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(o){return null==o?"":i._compress(o,6,function(o){return e.charAt(o)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(n){return o(e,r.charAt(n))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(o,r,n){if(null==o)return"";var e,t,i,s={},p={},u="",c="",a="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<o.length;i+=1)if(u=o.charAt(i),Object.prototype.hasOwnProperty.call(s,u)||(s[u]=f++,p[u]=!0),c=a+u,Object.prototype.hasOwnProperty.call(s,c))a=c;else{if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++),s[c]=f++,a=String(u)}if(""!==a){if(Object.prototype.hasOwnProperty.call(p,a)){if(a.charCodeAt(0)<256){for(e=0;h>e;e++)m<<=1,v==r-1?(v=0,d.push(n(m)),m=0):v++;for(t=a.charCodeAt(0),e=0;8>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;h>e;e++)m=m<<1|t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=a.charCodeAt(0),e=0;16>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}l--,0==l&&(l=Math.pow(2,h),h++),delete p[a]}else for(t=s[a],e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;l--,0==l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;h>e;e++)m=m<<1|1&t,v==r-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==r-1){d.push(n(m));break}v++}return d.join("")},decompress:function(o){return null==o?"":""==o?null:i._decompress(o.length,32768,function(r){return o.charCodeAt(r)})},_decompress:function(o,n,e){var t,i,s,p,u,c,a,l,f=[],h=4,d=4,m=3,v="",w=[],A={val:e(0),position:n,index:1};for(i=0;3>i;i+=1)f[i]=i;for(p=0,c=Math.pow(2,2),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(t=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;l=r(p);break;case 2:return""}for(f[3]=l,s=l,w.push(l);;){if(A.index>o)return"";for(p=0,c=Math.pow(2,m),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;switch(l=p){case 0:for(p=0,c=Math.pow(2,8),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 1:for(p=0,c=Math.pow(2,16),a=1;a!=c;)u=A.val&A.position,A.position>>=1,0==A.position&&(A.position=n,A.val=e(A.index++)),p|=(u>0?1:0)*a,a<<=1;f[d++]=r(p),l=d-1,h--;break;case 2:return w.join("")}if(0==h&&(h=Math.pow(2,m),m++),f[l])v=f[l];else{if(l!==d)return null;v=s+s.charAt(0)}w.push(v),f[d++]=s+v.charAt(0),h--,s=v,0==h&&(h=Math.pow(2,m),m++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module&&(module.exports=LZString);
     
     return obj;
 
---
+++ minify.bat
@@ -0,0 +1,1 @@
+java -jar yuicompressor-2.4.8.jar cbl.js -o cbl.min.js
